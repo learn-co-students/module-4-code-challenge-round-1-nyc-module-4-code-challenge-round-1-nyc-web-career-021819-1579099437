33259d5c6f2f5748a18065a02db7cdb4a9350745
crpineda1 <carlos.r.pineda@gmail.com>
2020-01-15 11:10:35 -0500

    checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -3,10 +3,28 @@ import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
   //your code here
+ bots = this.props.botCollection
+ 
+state ={
+	  allBots: this.bots,
+	  currentBot: null
+	  
+  }
+
+  componentDidMount() {
+	  this.setState({
+		  allBots: this.props.botCollection
+	  })
+  }
+
 
   renderBots = (bots) => {
 	// console.log(bots)  
-	return bots.map( each => <BotCard key = {each.id} bot = {each} handleClick = {this.props.addToArmy}/>)
+	return bots.map( each => <BotCard key = {each.id} bot = {each} handleClick = {this.renderBotSpecs}/>)
+  }
+
+  renderBotSpecs = (bot) => {
+	  console.log(bot)
   }
 
   render(){
@@ -15,6 +33,7 @@ class BotCollection extends React.Component {
     		<div className="row">
     		  {this.renderBots(this.props.botCollection)}
     		  Collection of all bots
+			  {/* {this.renderBotSpecs(this.State.currentBot)} */}
 
     		</div>
   	  </div>
